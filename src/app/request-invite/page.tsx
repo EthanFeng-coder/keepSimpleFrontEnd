035ce77eddb82a4254c6a91e4495b7d83ee53822
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2024-08-28 15:28:00 +1000

    feat: add request Invite; refine UI

---
+++ src/app/request-invite/page.tsx
@@ -0,0 +1,12 @@
+import RequestInvite from '@/components/RequestInvite'
+import { Header } from '@/components/Header'
+
+const RequestInvitePage = () => {
+  return (
+    <>
+      <RequestInvite />
+    </>
+  )
+}
+
+export default RequestInvitePage
