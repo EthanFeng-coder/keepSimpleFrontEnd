4585e1f07a8b3738ea1e202fd1f2c9fc87d8ad76
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2024-09-10 17:03:04 +1000

    feat: add basic doughnut chart

---
+++ src/app/chart/doughnut-chart/page.tsx
@@ -0,0 +1,14 @@
+// SomePage.tsx
+import React from 'react';
+import DoughnutChart from '@/components/DoughnutChart';
+
+const SomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>My Doughnut Chart</h1>
+      <DoughnutChart />
+    </div>
+  );
+};
+
+export default SomePage;
---
+++ src/components/DoughnutChart.tsx
@@ -0,0 +1,65 @@
+'use client'
+import React, { useEffect } from 'react'
+import * as echarts from 'echarts'
+
+type EChartsOption = echarts.EChartsOption
+
+const DoughnutChart: React.FC = () => {
+  useEffect(() => {
+    var chartDom = document.getElementById('main')!
+    var myChart = echarts.init(chartDom)
+
+    var option: EChartsOption = {
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        top: '5%',
+        left: 'center',
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: 'center',
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold',
+            },
+          },
+          labelLine: {
+            show: false,
+          },
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' },
+          ],
+        },
+      ],
+    }
+
+    option && myChart.setOption(option)
+
+    window.addEventListener('resize', () => {
+      myChart.resize()
+    })
+
+    return () => {
+      myChart.dispose()
+    }
+  }, [])
+
+  return <div id="main" style={{ width: '100%', height: '400px' }} />
+}
+
+export default DoughnutChart
