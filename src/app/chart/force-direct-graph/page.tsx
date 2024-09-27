fae4d7c5836a0f471eda41573f5b4cb3b11b4ee7
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2024-09-27 17:42:25 +1000

    add force direct graph

---
+++ src/app/chart/force-direct-graph/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GraphComponent from '@/components/ForceDirectGraph';
+
+const GraphPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Graph Visualization</h1>
+      <GraphComponent />
+    </div>
+  );
+};
+
+export default GraphPage;
